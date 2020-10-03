075cc7841bb6f0eac8b127351edf41b3f096b7a0
biequal <biequal@naver.com>
2020-10-03 08:08:57 +0000

    setting.py DEBUG=False,end

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h#ng93ix^^^-0$39t!7t#@w-za&l1+ocj86%pbhl!@dxrt*44w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
